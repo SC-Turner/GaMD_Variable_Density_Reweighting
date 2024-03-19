0eaef0c17e52248e8d055866ea4b7dca373a170f
S.C.Turner <sct1g15@cyan53.cluster.local>
2024-03-19 04:02:31 +0000

    PyPI setup

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ REQUIRES_PYTHON = '>=3.6'
 VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
-REQUIRED = ['numpy', 'MDAnalysis', 'scipy>=1.10.1', 'argparse', 'matplotlib', 'pandas', 'sklearn'
+REQUIRED = ['numpy', 'MDAnalysis', 'scipy>=1.10.1', 'argparse', 'matplotlib', 'pandas', 'scikit-learn', 'multiprocess'
     # 'requests', 'maya', 'records',
 ]
 
